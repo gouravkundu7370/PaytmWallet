dafaa137b574a2ab9781e125290b42ec1321450a
Gourav Kundu <gouravkundu2902@gmail.com>
2024-07-05 13:20:21 +0530

    Fixes merchant auth

--- apps/merchant-app/app/api/auth/[...nextauth]/route.ts
+++ apps/merchant-app/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
-import NextAuth from "next-auth/next";
-import { authOptions } from "../../../lib/auth";
+import NextAuth from "next-auth";
+import { authOptions } from "../../../../lib/auth";
 
+//@ts-ignore
 const handler = NextAuth(authOptions);
 
-export {handler as GET,handler as POST}
+export { handler as GET, handler as POST };
